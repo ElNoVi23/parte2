d58ef46cede9d6a384d5707a2f443d93ba39744f
Usuario <Usuario@host.docker.internal>
2021-03-01 18:04:19 +0100

    Revert "Tercera revisión"
    This reverts commit 2250b2bd2b558941eea0ee23d8d686740ce93dd2.

--- Proyecto_git/src/Hola_mundo.java
+++ Proyecto_git/src/Hola_mundo.java
@@ -2,6 +2,5 @@ public class Hola_mundo{
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo otra vez");
-		System.out.println("Adiós mundo");
 	}
 }
