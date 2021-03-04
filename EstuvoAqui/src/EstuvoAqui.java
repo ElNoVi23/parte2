5c99ca5c454417ee4f0ca97de1024d43e56f7a71
Usuario <Usuario@DESKTOP-DGNAOIQ.mshome.net>
2021-03-04 16:31:44 +0100

    primera revision

---
+++ EstuvoAqui/src/EstuvoAqui.java
@@ -0,0 +1,18 @@
+
+public class EstuvoAqui {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("JM estuvo aquí \n");
+		System.out.println("Dominik Novotny estuvo aquí \n");
+		System.out.println("Juan Pedro Belijar estuvo aquí \n");
+		System.out.println("Yolanda Muñoz Puerta estuvo aquí \n");
+		System.out.println("Gines Ferez Asensio estuvo aquí \n");
+		System.out.println("David Valcarcel estuvo aquí \n");
+                System.out.println("AntonioFernandez estuvo aqui \n");
+                System.out.println("Ihab Fallahy Aallam estuvo aqui \n");
+                System.out.println("Cristian Aldo Novelli estuvo aqui \n");
+                
+	}
+
+}
